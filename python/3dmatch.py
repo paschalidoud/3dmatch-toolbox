0447c698f51b72a4e21feef21781b337d61470de
Despoina Paschalidou <dpaschalidou@trieb.is.localnet>
2017-04-27 15:24:49 +0200

    Fix bug in fit_generator

--- python/3dmatch.py
+++ python/3dmatch.py
@@ -20,41 +20,43 @@ def generate_batches(input_directory, batch_size):
 
     tdf_grid_dimensions = 30*30*30
 
-    P1 = np.empty((0, tdf_grid_dimensions), dtype=np.float32)
-    P2 = np.empty((0, tdf_grid_dimensions), dtype=np.float32)
-    labels = []
-    # Choose a random index and a random offset to read from the input directory
-    for idx in range(batch_size):
-        random_idx = np.random.randint(0, len(p1))
-        random_offset = np.random.randint(0, 100)
-        #print idx,"/", batch_size
+    while True:
 
-        f1 = open(os.path.join(input_directory, p1[random_idx]))
-        f1.seek(random_offset * 4 * tdf_grid_dimensions)
-        d1 = np.fromfile(f1, count=tdf_grid_dimensions, dtype=np.float32).reshape(-1, tdf_grid_dimensions)
+        P1 = np.empty((0, tdf_grid_dimensions), dtype=np.float32)
+        P2 = np.empty((0, tdf_grid_dimensions), dtype=np.float32)
+        labels = []
+        # Choose a random index and a random offset to read from the input directory
+        for idx in range(batch_size):
+            random_idx = np.random.randint(0, len(p1))
+            random_offset = np.random.randint(0, 100)
+            #print idx,"/", batch_size
 
-        f2 = open(os.path.join(input_directory, p2[random_idx]))
-        f2.seek(random_offset * 4 * tdf_grid_dimensions)
-        d2 = np.fromfile(f2, count=tdf_grid_dimensions, dtype=np.float32).reshape(-1, tdf_grid_dimensions)
+            f1 = open(os.path.join(input_directory, p1[random_idx]))
+            f1.seek(random_offset * 4 * tdf_grid_dimensions)
+            d1 = np.fromfile(f1, count=tdf_grid_dimensions, dtype=np.float32).reshape(-1, tdf_grid_dimensions)
 
-        f3 = open(os.path.join(input_directory, p3[random_idx]))
-        f3.seek(random_offset * 4 * tdf_grid_dimensions)
-        d3 = np.fromfile(f3, count=tdf_grid_dimensions, dtype=np.float32).reshape(-1, tdf_grid_dimensions)
+            f2 = open(os.path.join(input_directory, p2[random_idx]))
+            f2.seek(random_offset * 4 * tdf_grid_dimensions)
+            d2 = np.fromfile(f2, count=tdf_grid_dimensions, dtype=np.float32).reshape(-1, tdf_grid_dimensions)
 
-        # Add the reference point
-        P1 = np.vstack((P1, d1))
-        P1 = np.vstack((P1, d1))
-        labels.append(1)
-        # Add the matching point and the non-matching point
-        P2 = np.vstack((P2, d2))
-        P2 = np.vstack((P2, d3))
-        labels.append(0)
+            f3 = open(os.path.join(input_directory, p3[random_idx]))
+            f3.seek(random_offset * 4 * tdf_grid_dimensions)
+            d3 = np.fromfile(f3, count=tdf_grid_dimensions, dtype=np.float32).reshape(-1, tdf_grid_dimensions)
 
-        f1.close()
-        f2.close()
-        f3.close()
+            # Add the reference point
+            P1 = np.vstack((P1, d1))
+            P1 = np.vstack((P1, d1))
+            labels.append(1)
+            # Add the matching point and the non-matching point
+            P2 = np.vstack((P2, d2))
+            P2 = np.vstack((P2, d3))
+            labels.append(0)
 
-    yield [[P1.reshape((-1, 30, 30, 30, 1)), P2.reshape((-1, 30, 30, 30, 1))], np.array(labels)] 
+            f1.close()
+            f2.close()
+            f3.close()
+
+        yield [[P1.reshape((-1, 30, 30, 30, 1)), P2.reshape((-1, 30, 30, 30, 1))], np.array(labels)] 
 
 def euclidean_distance(D):
     D1 = D[0]
