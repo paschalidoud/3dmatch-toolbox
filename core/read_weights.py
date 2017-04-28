0ef291eb7a7a5b8147fa0d1704a54e5b60a7ed43
Despoina Paschalidou <dpaschalidou@trieb.is.localnet>
2017-04-28 11:55:58 +0200

    Finish script to transform marvin weights/bias to keras format

--- core/read_weights.py
+++ core/read_weights.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import h5py
 import numpy as np
 
 def read_tensor(f):
@@ -25,10 +26,16 @@ def read_tensor(f):
         count=weights_shape[0]*weights_shape[1]*weights_shape[2]*weights_shape[3]*weights_shape[4],
         dtype=np.float32
     )
-    weights = weights.reshape(
-        (weights_shape[2], weights_shape[3], weights_shape[4], weights_shape[1], weights_shape[0])
-    )
-    #print weights
+    if "bias" in layer_name:
+        weights = weights.reshape(
+            (weights_shape[1])
+        )
+        
+    else:
+        weights = weights.reshape(
+            (weights_shape[2], weights_shape[3], weights_shape[4], weights_shape[1], weights_shape[0])
+        )
+    print weights.shape
     print " "
     return layer_name, weights
     
@@ -40,5 +47,64 @@ if __name__ == "__main__":
     for i in range(18):
         layer_name, weights = read_tensor(f)
         model[layer_name] = weights
+    
+    f = h5py.File("/tmp/3dmatch-weights-snapshot-137000.marvin", "w")
 
-    #print model
+    # Create inital group
+    group1 = f.create_group("model_weights")
+    # Create first subgroup
+    group21 = f["model_weights"].create_group("input_1")
+    group22 = f["model_weights"].create_group("input_2")
+    group23 = f["model_weights"].create_group("lambda_1")
+    group24 = f["model_weights"].create_group("sequential_1")
+    # Create second subgroup
+    g = f["model_weights"]
+    group31 = g["sequential_1"].create_group("conv3d_1")
+    group32 = g["sequential_1"].create_group("conv3d_2")
+    group33 = g["sequential_1"].create_group("conv3d_3")
+    group34 = g["sequential_1"].create_group("conv3d_4")
+    group35 = g["sequential_1"].create_group("conv3d_5")
+    group36 = g["sequential_1"].create_group("conv3d_6")
+    group37 = g["sequential_1"].create_group("conv3d_7")
+    group38 = g["sequential_1"].create_group("conv3d_8")
+    group39 = g["sequential_1"].create_group("conv3d_9")
+
+    # Start creating datasets
+    s = g["sequential_1"]
+    # First convolution layer
+    s["conv3d_1"].create_dataset("bias:0", (64,), data=model["conv1.bias"], dtype="f4")
+    s["conv3d_1"].create_dataset("kernel:0", (3, 3, 3, 1, 64), data=model["conv1.weight"], dtype="f4")
+
+    # Second convolution layer
+    s["conv3d_2"].create_dataset("bias:0", (64,), data=model["conv2.bias"], dtype="f4")
+    s["conv3d_2"].create_dataset("kernel:0", (3, 3, 3, 64, 64), data=model["conv2.weight"], dtype="f4")
+
+    # Third convolution layer
+    s["conv3d_3"].create_dataset("bias:0", (128,), data=model["conv3.bias"], dtype="f4")
+    s["conv3d_3"].create_dataset("kernel:0", (3, 3, 3, 64, 128), data=model["conv3.weight"], dtype="f4")
+
+    # Fourth convolution layer
+    s["conv3d_4"].create_dataset("bias:0", (128,), data=model["conv4.bias"], dtype="f4")
+    s["conv3d_4"].create_dataset("kernel:0", (3, 3, 3, 128, 128), data=model["conv4.weight"], dtype="f4")
+
+    # Fifth convolution layer
+    s["conv3d_5"].create_dataset("bias:0", (256,), data=model["conv5.bias"], dtype="f4")
+    s["conv3d_5"].create_dataset("kernel:0", (3, 3, 3, 128, 256), data=model["conv5.weight"], dtype="f4")
+
+    # Sixth convolution layer
+    s["conv3d_6"].create_dataset("bias:0", (256,), data=model["conv6.bias"], dtype="f4")
+    s["conv3d_6"].create_dataset("kernel:0", (3, 3, 3, 256, 256), data=model["conv6.weight"], dtype="f4")
+
+    # Seventh convolution layer
+    s["conv3d_7"].create_dataset("bias:0", (512,), data=model["conv7.bias"], dtype="f4")
+    s["conv3d_7"].create_dataset("kernel:0", (3, 3, 3, 256, 512), data=model["conv7.weight"], dtype="f4")
+
+    # Eighth convolution layer
+    s["conv3d_8"].create_dataset("bias:0", (512,), data=model["conv8.bias"], dtype="f4")
+    s["conv3d_8"].create_dataset("kernel:0", (3, 3, 3, 512, 512), data=model["conv8.weight"], dtype="f4")
+    
+    # Ninth convolution layer
+    s["conv3d_9"].create_dataset("bias:0", (512,), data=model["feat.bias"], dtype="f4")
+    s["conv3d_9"].create_dataset("kernel:0", (1, 1, 1, 512, 512), data=model["feat.weight"], dtype="f4")
+
+    f.close()
